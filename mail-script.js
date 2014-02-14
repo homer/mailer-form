d7c6efb30700b07bc541cede7e859feef18e2911
H. Ömer Şensoy <hosensoy@gmail.com>
2014-02-14 09:42:22 +0200

    Regex update for email validation
    Email daha komplike emailler icin filtreleme yapamiyordu. meltem.parlak@gmail.com, meltem_parlak@gmail.com, meltem+parlak@gmail.com veya meltem.parlak@gmail.com.tr gibi emailleri de algilayabilecek bir regex yaptim.,

---
+++ mail-script.js
@@ -0,0 +1,51 @@
+$(function(){
+    // not enough
+    // var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    // better version
+    // var pattern = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/i;
+    // best I have found so far
+    var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    var aTextbox;
+    var aMail;
+    var $mailsInput = $("#mailsInput");
+
+    $mailsInput.tagsInput({
+        defaultText:'',
+        width:'335px'
+    });
+
+    $("#mailer").on("click",function(){
+        $(".mailer-canvas").removeClass("is-hidden");
+    });
+
+    $("#form-submit").on("click",function(e){
+        e.preventDefault();
+        makeMailsArray($mailsInput.val());
+        inputMailVals(aMail);
+        testEmailsFromConsoleLog();
+    });
+    function pushEmails(str) {
+        aTemp = str.match(pattern);
+        if(aTemp != null){
+            aMail.push(aTemp[0]);
+        }
+    }
+    function makeMailsArray(mailString){
+        aTextbox = mailString.split(",");
+        $.each(aTextbox,function(i,val) {
+            pushEmails(val);
+        });
+    }
+    function inputMailVals(mailsArray) {
+        var emailsVal = mailsArray.join();
+        $mailsInput.val(emailsVal);
+    }
+    function testEmailsFromConsoleLog(){
+        console.log("aTextbox array:");
+        console.log(aTextbox);
+        console.log("aMail array:");
+        console.log(aMail);
+        console.log("input field val:");
+        console.log($mailsInput.val());
+    }
+});
